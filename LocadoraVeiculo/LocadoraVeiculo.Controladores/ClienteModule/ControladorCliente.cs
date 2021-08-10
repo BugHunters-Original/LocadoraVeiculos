6faccdb7feda8f242679f3c9f00ab806c7ca1da3
GabrielMrqs <gabrielmrqs.contato@gmail.com>
2021-08-10 15:34:18 -0300

    Adicionado ControladorCliente

--- LocadoraVeiculo/LocadoraVeiculo.Controladores/ClienteModule/ControladorCliente.cs
+++ LocadoraVeiculo/LocadoraVeiculo.Controladores/ClienteModule/ControladorCliente.cs
@@ -5,39 +5,159 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace LocadoraVeiculo.Controladores.ClienteModule
 {
     public class ControladorCliente : Controlador<Cliente>
     {
+        private const string sqlInserirCliente =
+       @"INSERT INTO TBCONTATO 
+	                (
+		                [NOME], 
+		                [ENDERECO], 
+		                [TELEFONE],
+                        [CPF_CNPJ], 
+		                [TIPO]
+	                ) 
+	                VALUES
+	                (
+                        @NOME, 
+                        @ENDERECO,
+                        @TELEFONE,
+		                @CPF_CNPJ, 
+		                @TIPO
+	                )";
+
+        private const string sqlEditarCliente =
+            @"UPDATE TBCLIENTE
+                    SET
+                        [NOME] = @NOME,
+		                [ENDERECO] = @ENDERECO, 
+		                [TELEFONE] = @TELEFONE,
+                        [CPF_CNPJ] = @CPF_CNPJ,
+                        [TIPO] = @TIPO
+                    WHERE 
+                        ID = @ID";
+
+        private const string sqlExcluirCliente =
+            @"DELETE 
+	                FROM
+                        TBCLIENTE
+                    WHERE 
+                        ID = @ID";
+
+        private const string sqlSelecionarClientePorId =
+            @"SELECT
+                        [ID],
+		                [NOME], 
+		                [ENDERECO], 
+		                [TELEFONE],
+                        [CPF_CNPJ], 
+		                [TIPO]
+	                FROM
+                        TBCLIENTE
+                    WHERE 
+                        ID = @ID";
+
+        private const string sqlSelecionarTodosClientes =
+            @"SELECT
+                        [ID],
+		                [NOME], 
+		                [ENDERECO], 
+		                [TELEFONE],
+                        [CPF_CNPJ], 
+		                [TIPO]
+	                FROM
+                        TBCLIENTE";
+
+        private const string sqlExisteCliente =
+            @"SELECT 
+                COUNT(*) 
+            FROM 
+                [TBCLIENTE]
+            WHERE 
+                [ID] = @ID";
         public override string Editar(int id, Cliente registro)
         {
-            throw new NotImplementedException();
+            string resultadoValidacao = registro.Validar();
+
+            if (resultadoValidacao == "ESTA_VALIDO")
+            {
+                registro.Id = id;
+                Db.Update(sqlEditarCliente, ObtemParametrosCliente(registro));
+            }
+
+            return resultadoValidacao;
         }
 
         public override bool Excluir(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Db.Delete(sqlExcluirCliente, AdicionarParametro("ID", id));
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
         }
 
         public override bool Existe(int id)
         {
-            throw new NotImplementedException();
+            return Db.Exists(sqlExisteCliente, AdicionarParametro("ID", id));
         }
 
         public override string InserirNovo(Cliente registro)
         {
-            throw new NotImplementedException();
+            string resultadoValidacao = registro.Validar();
+
+            if (resultadoValidacao == "ESTA_VALIDO")
+            {
+                registro.Id = Db.Insert(sqlInserirCliente, ObtemParametrosCliente(registro));
+            }
+
+            return resultadoValidacao;
         }
 
         public override Cliente SelecionarPorId(int id)
         {
-            throw new NotImplementedException();
+            return Db.Get(sqlSelecionarClientePorId, ConverterEmCliente, AdicionarParametro("ID", id));
         }
 
         public override List<Cliente> SelecionarTodos()
         {
-            throw new NotImplementedException();
+            return Db.GetAll(sqlSelecionarTodosClientes, ConverterEmCliente);
+        }
+        private Dictionary<string, object> ObtemParametrosCliente(Cliente cliente)
+        {
+            var parametros = new Dictionary<string, object>();
+
+            parametros.Add("ID", cliente.Id);
+            parametros.Add("NOME", cliente.Nome);
+            parametros.Add("ENDERECO", cliente.Endereco);
+            parametros.Add("TELEFONE", cliente.Telefone);
+            parametros.Add("CPF_CNPJ", cliente.Cpf_cnpj);
+            parametros.Add("TIPO", cliente.Tipo);
+
+            return parametros;
+        }
+        private Cliente ConverterEmCliente(IDataReader reader)
+        {
+            int id = Convert.ToInt32(reader["ID"]);
+            string nome = Convert.ToString(reader["NOME"]);
+            string endereco = Convert.ToString(reader["ENDERECO"]);
+            string telefone = Convert.ToString(reader["TELEFONE"]);
+            string cpf_cnpj = Convert.ToString(reader["CPF_CNPJ"]);
+            string tipo = Convert.ToString(reader["TIPO"]);
+
+            Cliente cliente = new Cliente(nome, endereco, telefone, cpf_cnpj, tipo);
+
+            cliente.Id = id;
+
+            return cliente;
         }
     }
 }
