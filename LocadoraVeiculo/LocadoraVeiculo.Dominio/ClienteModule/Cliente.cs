cc6823e8d4110350f3c45a37c921dbfbdf80125a
GabrielMrqs <gabrielmrqs.contato@gmail.com>
2021-08-10 15:16:39 -0300

    Feito o Domínio Cliente

--- LocadoraVeiculo/LocadoraVeiculo.Dominio/ClienteModule/Cliente.cs
+++ LocadoraVeiculo/LocadoraVeiculo.Dominio/ClienteModule/Cliente.cs
@@ -1,4 +1,5 @@
-﻿using LocadoraVeiculo.Shared;
+﻿using LocadoraVeiculo.CondutorModule;
+using LocadoraVeiculo.Shared;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,47 @@ namespace LocadoraVeiculo.ClienteModule
 {
     public class Cliente : EntidadeBase
     {
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+        public string Cpf_cnpj { get; set; }
+        public string Tipo { get; set; }
+        public Cliente(string nome, string endereco, string telefone, string cpf_cnpj, string tipo)
+        {
+            Nome = nome;
+            Endereco = endereco;
+            Telefone = telefone;
+            Cpf_cnpj = cpf_cnpj;
+            Tipo = tipo;
+        }
+
         public override string Validar()
         {
-            throw new NotImplementedException();
+            string valido = "";
+
+            if (string.IsNullOrEmpty(Nome))
+                valido += "Nome em branco";
+
+            if (string.IsNullOrEmpty(Endereco))
+                valido += QuebraDeLinha(valido) + "Endereço em branco";
+
+            if (string.IsNullOrEmpty(Telefone) || Telefone.Length != 14)
+                valido += QuebraDeLinha(valido) + "Telefone em branco";
+
+            if (string.IsNullOrEmpty(Cpf_cnpj))
+                valido += QuebraDeLinha(valido) + "CPF ou CNPJ em branco";
+
+            if (string.IsNullOrEmpty(Tipo))
+                valido += QuebraDeLinha(valido) + "Tipo em branco";
+
+            if (valido == "")
+                valido = "ESTA_VALIDO";
+
+            return valido;
+        }
+        public override string ToString()
+        {
+            return Nome;
         }
     }
 }
---
+++ LocadoraVeiculo/LocadoraVeiculo.Dominio/CondutorModule/Condutor.cs
@@ -0,0 +1,17 @@
+﻿using LocadoraVeiculo.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraVeiculo.CondutorModule
+{
+    public class Condutor : EntidadeBase
+    {
+        public override string Validar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
