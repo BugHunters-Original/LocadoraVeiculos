1a0eb96cfdf6dd99e7e347047b5f12b59e2b01a1
Andrey Silva <AndreyFreitag2001@outlook.com>
2021-08-11 17:18:44 -0300

    Implementado Tests GrupoVeiculo

---
+++ LocadoraVeiculo/LocadoraVeiculo.Tests/GrupoVeiculoModule/GrupoVeiculoTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using LocadoraVeiculo.GrupoVeiculoModule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LocadoraVeiculo.Tests.GrupoVeiculoModule
+{
+    [TestClass]
+    public class GrupoVeiculoTests
+    {
+        [TestMethod]
+        public void DeveValidar_GrupoVeiculo()
+        {
+            GrupoVeiculo grupoVeiculo = new GrupoVeiculo("Econômico", 40, 5, 50, 30, 40);
+
+            //action
+            var resultadoValidacao = grupoVeiculo.Validar();
+
+            //assert
+            resultadoValidacao.Should().Be("ESTA_VALIDO");
+        }
+
+        [TestMethod]
+        public void DeveRetornar_ErroSemNome()
+        {
+            GrupoVeiculo grupoVeiculo = new GrupoVeiculo("", 40, 5, 50, 30, 40);
+
+            //action
+            var resultadoValidacao = grupoVeiculo.Validar();
+
+            //assert
+            resultadoValidacao.Should().Be("O campo categoria é obrigatório");
+        }
+    }
+}
