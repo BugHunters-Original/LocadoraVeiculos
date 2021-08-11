53989dd9ae642a11f8410dd4f17e00c4457ff379
arthurmichaltchuk <arthurmichaltchuk4@gmail.com>
2021-08-11 15:45:19 -0300

    Criação dos UserControl e da Feature funcionário

---
+++ LocadoraVeiculo/LocadoraVeiculo.WindowsApp/Features/Funcionario/OperacoesFuncionario.cs
@@ -0,0 +1,54 @@
+﻿using LocadoraVeiculo.Controladores.FuncionarioModule;
+using LocadoraVeiculo.WindowsApp.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocadoraVeiculo.WindowsApp.Features.Funcionario
+{
+    public class OperacoesFuncionario : ICadastravel
+    {
+        private readonly ControladorFuncionario controlador = null;
+        //private readonly TabelaTarefaControl tabelaTarefas = null;
+
+        public OperacoesFuncionario(ControladorFuncionario ctrlLocacao)
+        {
+            controlador = ctrlLocacao;
+            //tabelaTarefas = new TabelaTarefaControl();
+        }
+
+
+        public void DevolverVeiculo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditarRegistro()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExcluirRegistro()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FiltrarRegistros()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InserirNovoRegistro()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserControl ObterTabela()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
