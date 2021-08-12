5fdb81efc52f8a6a50233c24c5d051aaf94ee9d9
GabrielMrqs <gabrielmrqs.contato@gmail.com>
2021-08-12 17:57:38 -0300

    Implemetnação Testes Serviços

--- LocadoraVeiculo/LocadoraVeiculo.Dominio/ServicoModule/Servico.cs
+++ LocadoraVeiculo/LocadoraVeiculo.Dominio/ServicoModule/Servico.cs
@@ -24,7 +24,7 @@ namespace LocadoraVeiculo.ServicoModule
             if (Preco <= 0)
                 valido += QuebraDeLinha(valido) + "O campo Preço está inválido";
 
-            if (TipoCalculo != 1 || TipoCalculo != 0)
+            if (TipoCalculo != 1 && TipoCalculo != 0)
                 valido += QuebraDeLinha(valido) + "O campo Tipo está inválido";
 
             if (valido == "")
