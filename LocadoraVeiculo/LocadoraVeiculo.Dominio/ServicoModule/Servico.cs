ed03f3477deda79dd205ddcb9a58f877a86deed6
GabrielMrqs <gabrielmrqs.contato@gmail.com>
2021-08-12 18:26:42 -0300

    Fix Testes Serviços

--- LocadoraVeiculo/LocadoraVeiculo.Dominio/ServicoModule/Servico.cs
+++ LocadoraVeiculo/LocadoraVeiculo.Dominio/ServicoModule/Servico.cs
@@ -1,9 +1,10 @@
 ﻿using LocadoraVeiculo.Shared;
 using System;
+using System.Collections.Generic;
 
 namespace LocadoraVeiculo.ServicoModule
 {
-    public class Servico : EntidadeBase
+    public class Servico : EntidadeBase, IEquatable<Servico>
     {
         public Servico(string nome, decimal preco, int tipoCalculo)
         {
@@ -35,6 +36,29 @@ namespace LocadoraVeiculo.ServicoModule
         public override string ToString()
         {
             return Nome;
+        }
+
+        public bool Equals(Servico other)
+        {
+            return other != null
+                && Id == other.Id
+                && Nome == other.Nome
+                && Preco == other.Preco
+                && TipoCalculo == other.TipoCalculo;
+        }
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Servico);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -549020184;
+            hashCode = hashCode * -1521134295 + Id.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Nome);
+            hashCode = hashCode * -1521134295 + Preco.GetHashCode();
+            hashCode = hashCode * -1521134295 + TipoCalculo.GetHashCode();
+            return hashCode;
         }
     }
 }
