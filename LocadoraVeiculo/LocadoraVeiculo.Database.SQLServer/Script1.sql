ff2f20d29d0bd8598b2b5d17079ed84ac5d41a79
GabrielMrqs <gabrielmrqs.contato@gmail.com>
2021-08-12 13:49:52 -0300

    Script Atualizado

--- LocadoraVeiculo/LocadoraVeiculo.Database.SQLServer/Script1.sql
+++ LocadoraVeiculo/LocadoraVeiculo.Database.SQLServer/Script1.sql
@@ -1,22 +1,8 @@
-﻿select * from TBCliente
+﻿select * from TBClienteCNPJ
+delete from TBClienteCNPJ
+DBCC CHECKIDENT ( TBClienteCNPJ, RESEED )
 
-delete from TBCliente
 
-DBCC CHECKIDENT ( TBCliente, RESEED )
-
-INSERT INTO TBCLIENTE 
-	                (
-		                [NOME], 
-		                [ENDERECO], 
-		                [TELEFONE],
-                        [CPF_CNPJ], 
-		                [TIPO]
-	                ) 
-	                VALUES
-	                (
-                        'Gabriel Marques',
-						'Guarujá',
-						'(49)99803-5074',
-						'01190011956',
-						'PF'
-	                );
+select * from TBClienteCPF
+delete from TBClienteCPF
+DBCC CHECKIDENT ( TBClienteCPF, RESEED )
