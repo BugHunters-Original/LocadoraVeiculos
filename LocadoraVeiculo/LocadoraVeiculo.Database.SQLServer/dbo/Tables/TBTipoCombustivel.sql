f0804418d1ce6985a04a9f01a3ad1e48e5c0b7eb
Andrey Silva <andrey_silva13@outlook.com>
2021-08-12 16:48:24 -0300

    Alterações no banco

--- LocadoraVeiculo/LocadoraVeiculo.Database.SQLServer/dbo/Tables/TBTipoCombustivel.sql
+++ LocadoraVeiculo/LocadoraVeiculo.Database.SQLServer/dbo/Tables/TBTipoCombustivel.sql
@@ -1,7 +1,9 @@
 ﻿CREATE TABLE [dbo].[TBTipoCombustivel] (
     [Preco_Gasolina] DECIMAL (18) NULL,
     [Preco_Diesel]   DECIMAL (18) NULL,
-    [Preco_Alcool]   DECIMAL (18) NULL
+    [Preco_Alcool]   DECIMAL (18) NULL, 
+    [Id] INT NOT NULL IDENTITY, 
+    CONSTRAINT [PK_TBTipoCombustivel] PRIMARY KEY ([Id])
 );
 
 
