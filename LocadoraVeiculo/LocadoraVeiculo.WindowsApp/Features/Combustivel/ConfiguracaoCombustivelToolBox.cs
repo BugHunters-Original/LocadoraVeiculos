cb727be2466a4f424da75544cf9a97182f3eb7f8
Andrey Silva <andrey_silva13@outlook.com>
2021-08-12 15:48:24 -0300

    Implementado WindowsApp combustivel

---
+++ LocadoraVeiculo/LocadoraVeiculo.WindowsApp/Features/Combustivel/ConfiguracaoCombustivelToolBox.cs
@@ -0,0 +1,51 @@
+﻿using LocadoraVeiculo.WindowsApp.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraVeiculo.WindowsApp.Features.Combustivel
+{
+    class ConfiguracaoCombustivelToolBox : IConfiguracaoToolBox
+    {
+        public string ToolTipAdicionar
+        {
+            get { return ""; }
+        }
+        public string TipoCadastro
+        {
+            get { return "Configuração de combustíveis"; }
+        }
+
+        public string ToolTipEditar
+        {
+            get { return "Editar o preço de combustíveis"; }
+        }
+
+        public string ToolTipExcluir
+        {
+            get { return ""; }
+        }
+
+        public string ToolTipFiltrar
+        {
+            get { return ""; }
+        }
+
+        public string ToolTipDevolver
+        {
+            get { return ""; }
+        }
+
+        public bool EnabledFiltrar
+        {
+            get { return false; }
+        }
+
+        public bool EnabledDevolver
+        {
+            get { return false; }
+        }
+    }
+}
