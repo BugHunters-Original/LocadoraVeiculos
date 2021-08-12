a722d1dc989834468dd5d3973b9d78aefcbfb0d9
GabrielMrqs <gabrielmrqs.contato@gmail.com>
2021-08-12 12:04:42 -0300

    Implementação Taxas e Serviços

---
+++ LocadoraVeiculo/LocadoraVeiculo.WindowsApp/Features/TaxaServico/TelaTaxaServicoForm.Designer.cs
@@ -0,0 +1,46 @@
+﻿
+namespace LocadoraVeiculo.WindowsApp.Features.TaxaServico
+{
+    partial class TelaTaxaServicoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // TelaTaxaServicoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "TelaTaxaServicoForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
