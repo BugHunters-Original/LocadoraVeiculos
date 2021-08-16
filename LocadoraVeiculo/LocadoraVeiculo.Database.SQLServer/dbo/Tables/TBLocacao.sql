b092cb68bbecaefadd0ffe4ef9e944daee70426b
GabrielMrqs <gabrielmrqs.contato@gmail.com>
2021-08-15 21:44:06 -0300

    Fix Bug Cadastro Locacao ClienteCPF

--- LocadoraVeiculo/LocadoraVeiculo.Database.SQLServer/dbo/Tables/TBLocacao.sql
+++ LocadoraVeiculo/LocadoraVeiculo.Database.SQLServer/dbo/Tables/TBLocacao.sql
@@ -1,18 +1,18 @@
 ﻿CREATE TABLE [dbo].[TBLocacao] (
-    [Id]                   INT          IDENTITY (1, 1) NOT NULL,
-    [Id_Condutor]          INT          NULL,
-    [Id_Cliente]           INT          NULL,
-    [Id_Veiculo]           INT          NULL,
-    [Data_saida]           DATE         NULL,
-    [Data_retornoEsperado] DATE         NULL,
-    [Data_retornoChegada]  DATE         NULL,
-    [Valor_Caucao]         DECIMAL   NULL,
-    [Plano]                VARCHAR (50) NULL,
+    [Id]                   INT  IDENTITY (1, 1) NOT NULL,
+    [Id_Condutor]          INT  NULL,
+    [Id_ClienteLocador]    INT  NULL,
+    [Id_Veiculo]           INT  NULL,
+    [Data_saida]           DATE NULL,
+    [Data_retornoEsperado] DATE NULL,
+    [Plano]                INT  NULL,
+    [TipoCliente]          INT  NULL,
     CONSTRAINT [PK_TBLocacao] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_TBLocacao_Id_Condutor] FOREIGN KEY ([Id_Condutor]) REFERENCES [dbo].[TBClienteCPF] ([Id]),
-    CONSTRAINT [FK_TBLocacao_TBCliente] FOREIGN KEY ([Id_Cliente]) REFERENCES [dbo].[TBClienteCNPJ] ([Id]),
     CONSTRAINT [FK_TBLocacao_TBVeiculos] FOREIGN KEY ([Id_Veiculo]) REFERENCES [dbo].[TBVeiculos] ([Id])
 );
+
+
 
 
 
