9aeb36612549a9feef6ddc8fc161c78e3f8880ed
arthurmichaltchuk <arthurmichaltchuk4@gmail.com>
2021-08-20 15:00:06 -0300

    TelaTaxas DarkMode

--- LocadoraVeiculo/LocadoraVeiculo.WindowsApp/Features/Locacao/TelaDevolucaoForm.Designer.cs
+++ LocadoraVeiculo/LocadoraVeiculo.WindowsApp/Features/Locacao/TelaDevolucaoForm.Designer.cs
@@ -29,6 +29,7 @@ namespace LocadoraVeiculo.WindowsApp.Features.Locacao
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelaDevolucaoForm));
             this.dtRetorno = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
@@ -50,39 +51,44 @@ namespace LocadoraVeiculo.WindowsApp.Features.Locacao
             this.label4 = new System.Windows.Forms.Label();
             this.txtMulta = new System.Windows.Forms.TextBox();
             this.label9 = new System.Windows.Forms.Label();
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
             this.SuspendLayout();
             // 
             // dtRetorno
             // 
-            this.dtRetorno.Location = new System.Drawing.Point(147, 56);
+            this.dtRetorno.Location = new System.Drawing.Point(196, 69);
+            this.dtRetorno.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dtRetorno.Name = "dtRetorno";
-            this.dtRetorno.Size = new System.Drawing.Size(279, 20);
+            this.dtRetorno.Size = new System.Drawing.Size(371, 22);
             this.dtRetorno.TabIndex = 0;
             this.dtRetorno.ValueChanged += new System.EventHandler(this.dtRetorno_ValueChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(52, 62);
+            this.label1.Location = new System.Drawing.Point(69, 76);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.Size = new System.Drawing.Size(117, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Data de Retorno:";
             // 
             // txtKmAtual
             // 
-            this.txtKmAtual.Location = new System.Drawing.Point(147, 109);
+            this.txtKmAtual.Location = new System.Drawing.Point(196, 134);
+            this.txtKmAtual.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtKmAtual.Name = "txtKmAtual";
-            this.txtKmAtual.Size = new System.Drawing.Size(279, 20);
+            this.txtKmAtual.Size = new System.Drawing.Size(371, 22);
             this.txtKmAtual.TabIndex = 2;
             this.txtKmAtual.Leave += new System.EventHandler(this.txtKmAtual_Leave);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(34, 112);
+            this.label2.Location = new System.Drawing.Point(45, 138);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.Size = new System.Drawing.Size(144, 17);
             this.label2.TabIndex = 3;
             this.label2.Text = "Quilometragem Atual:";
             // 
@@ -99,61 +105,68 @@ namespace LocadoraVeiculo.WindowsApp.Features.Locacao
             "3/4",
             "7/8",
             "Cheio"});
-            this.cbNivelTanque.Location = new System.Drawing.Point(147, 136);
+            this.cbNivelTanque.Location = new System.Drawing.Point(196, 167);
+            this.cbNivelTanque.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cbNivelTanque.Name = "cbNivelTanque";
-            this.cbNivelTanque.Size = new System.Drawing.Size(279, 21);
+            this.cbNivelTanque.Size = new System.Drawing.Size(371, 24);
             this.cbNivelTanque.TabIndex = 4;
             this.cbNivelTanque.SelectedIndexChanged += new System.EventHandler(this.cbNivelTanque_SelectedIndexChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(50, 139);
+            this.label3.Location = new System.Drawing.Point(67, 171);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.Size = new System.Drawing.Size(116, 17);
             this.label3.TabIndex = 5;
             this.label3.Text = "Nível do Tanque:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(72, 166);
+            this.label5.Location = new System.Drawing.Point(96, 204);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.Size = new System.Drawing.Size(88, 17);
             this.label5.TabIndex = 7;
             this.label5.Text = "Combustível:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(105, 244);
+            this.label6.Location = new System.Drawing.Point(140, 300);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.Size = new System.Drawing.Size(44, 17);
             this.label6.TabIndex = 8;
             this.label6.Text = "Total:";
             // 
             // txtTotal
             // 
-            this.txtTotal.Location = new System.Drawing.Point(147, 241);
+            this.txtTotal.Location = new System.Drawing.Point(196, 297);
+            this.txtTotal.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtTotal.Name = "txtTotal";
             this.txtTotal.ReadOnly = true;
-            this.txtTotal.Size = new System.Drawing.Size(279, 20);
+            this.txtTotal.Size = new System.Drawing.Size(371, 22);
             this.txtTotal.TabIndex = 9;
             // 
             // txtCombustivel
             // 
-            this.txtCombustivel.Location = new System.Drawing.Point(147, 163);
+            this.txtCombustivel.Location = new System.Drawing.Point(196, 201);
+            this.txtCombustivel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCombustivel.Name = "txtCombustivel";
             this.txtCombustivel.ReadOnly = true;
-            this.txtCombustivel.Size = new System.Drawing.Size(279, 20);
+            this.txtCombustivel.Size = new System.Drawing.Size(371, 22);
             this.txtCombustivel.TabIndex = 11;
             // 
             // btnNota
             // 
             this.btnNota.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.btnNota.Location = new System.Drawing.Point(270, 276);
+            this.btnNota.Location = new System.Drawing.Point(360, 340);
+            this.btnNota.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnNota.Name = "btnNota";
-            this.btnNota.Size = new System.Drawing.Size(75, 23);
+            this.btnNota.Size = new System.Drawing.Size(100, 28);
             this.btnNota.TabIndex = 12;
             this.btnNota.Text = "Gerar Nota";
             this.btnNota.UseVisualStyleBackColor = true;
@@ -162,9 +175,10 @@ namespace LocadoraVeiculo.WindowsApp.Features.Locacao
             // btnCancelar
             // 
             this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancelar.Location = new System.Drawing.Point(351, 276);
+            this.btnCancelar.Location = new System.Drawing.Point(468, 340);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.Size = new System.Drawing.Size(100, 28);
             this.btnCancelar.TabIndex = 13;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = true;
@@ -172,76 +186,89 @@ namespace LocadoraVeiculo.WindowsApp.Features.Locacao
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(34, 85);
+            this.label7.Location = new System.Drawing.Point(45, 105);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(110, 13);
+            this.label7.Size = new System.Drawing.Size(147, 17);
             this.label7.TabIndex = 15;
             this.label7.Text = "Quilometragem Inicial:";
             // 
             // txtKmInicial
             // 
-            this.txtKmInicial.Location = new System.Drawing.Point(147, 82);
+            this.txtKmInicial.Location = new System.Drawing.Point(196, 101);
+            this.txtKmInicial.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtKmInicial.Name = "txtKmInicial";
             this.txtKmInicial.ReadOnly = true;
-            this.txtKmInicial.Size = new System.Drawing.Size(279, 20);
+            this.txtKmInicial.Size = new System.Drawing.Size(371, 22);
             this.txtKmInicial.TabIndex = 14;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(52, 36);
+            this.label8.Location = new System.Drawing.Point(69, 44);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(92, 13);
+            this.label8.Size = new System.Drawing.Size(121, 17);
             this.label8.TabIndex = 17;
             this.label8.Text = "Data de Previsão:";
             // 
             // dtRetornoEsperada
             // 
             this.dtRetornoEsperada.Enabled = false;
-            this.dtRetornoEsperada.Location = new System.Drawing.Point(147, 30);
+            this.dtRetornoEsperada.Location = new System.Drawing.Point(196, 37);
+            this.dtRetornoEsperada.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dtRetornoEsperada.Name = "dtRetornoEsperada";
-            this.dtRetornoEsperada.Size = new System.Drawing.Size(279, 20);
+            this.dtRetornoEsperada.Size = new System.Drawing.Size(371, 22);
             this.dtRetornoEsperada.TabIndex = 16;
             // 
             // txtServico
             // 
-            this.txtServico.Location = new System.Drawing.Point(147, 189);
+            this.txtServico.Location = new System.Drawing.Point(196, 233);
+            this.txtServico.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtServico.Name = "txtServico";
             this.txtServico.ReadOnly = true;
-            this.txtServico.Size = new System.Drawing.Size(279, 20);
+            this.txtServico.Size = new System.Drawing.Size(371, 22);
             this.txtServico.TabIndex = 19;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(88, 192);
+            this.label4.Location = new System.Drawing.Point(117, 236);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.Size = new System.Drawing.Size(66, 17);
             this.label4.TabIndex = 18;
             this.label4.Text = "Serviços:";
             // 
             // txtMulta
             // 
-            this.txtMulta.Location = new System.Drawing.Point(147, 215);
+            this.txtMulta.Location = new System.Drawing.Point(196, 265);
+            this.txtMulta.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtMulta.Name = "txtMulta";
             this.txtMulta.ReadOnly = true;
-            this.txtMulta.Size = new System.Drawing.Size(279, 20);
+            this.txtMulta.Size = new System.Drawing.Size(371, 22);
             this.txtMulta.TabIndex = 21;
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(103, 218);
+            this.label9.Location = new System.Drawing.Point(137, 268);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(36, 13);
+            this.label9.Size = new System.Drawing.Size(46, 17);
             this.label9.TabIndex = 20;
             this.label9.Text = "Multa:";
             // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 8;
+            this.bunifuElipse1.TargetControl = this;
+            // 
             // TelaDevolucaoForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(474, 311);
+            this.ClientSize = new System.Drawing.Size(632, 383);
             this.Controls.Add(this.txtMulta);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.txtServico);
@@ -262,7 +289,9 @@ namespace LocadoraVeiculo.WindowsApp.Features.Locacao
             this.Controls.Add(this.txtKmAtual);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dtRetorno);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "TelaDevolucaoForm";
@@ -296,5 +325,6 @@ namespace LocadoraVeiculo.WindowsApp.Features.Locacao
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox txtMulta;
         private System.Windows.Forms.Label label9;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
     }
 }
