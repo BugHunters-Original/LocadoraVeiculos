7b0bd6e05e69f4d737155c6da3bdbf74d1995b03
arthurmichaltchuk <arthurmichaltchuk4@gmail.com>
2021-08-20 14:13:09 -0300

    Refatoração ControladorDarkMode

--- LocadoraVeiculo/LocadoraVeiculo.WindowsApp/Features/DarkMode/ControladorDarkMode.cs
+++ LocadoraVeiculo/LocadoraVeiculo.WindowsApp/Features/DarkMode/ControladorDarkMode.cs
@@ -38,43 +38,49 @@ namespace LocadoraVeiculo.WindowsApp.Features.DarkMode
         public static void TrocarModo()
         {
             if (ligado == false)
-            {
-                imgAdd = Resources.outline_add_circle_outline_black_24dp;
-                imgEditar = Resources.outline_mode_edit_black_24dp;
-                imgExcluir = Resources.outline_delete_black_24dp;
-                imgFiltro = Resources.outline_filter_alt_black_24dp;
-                imgDevolver = Resources.devolver;
-                imgModo = Resources.whiteMode;
-
-                imgClose = Resources.close;
-                corGrid = Color.FromArgb(171, 171, 171);
-                corDark = Color.FromArgb(255, 255, 255);
-                corFonte = Color.Black;
-                corFundo = Color.FromArgb(242, 242, 242);
-                corFundoTxBox = Color.White;
-                corPanel = Color.FromArgb(191, 191, 191);
-                corTabela = Color.FromArgb(191, 191, 191);
-                ligado = true;
-            }
+                SetarModoClaro();
             else
-            {
-                imgAdd = Resources.outline_add_circle_outline_white_24dp;
-                imgEditar = Resources.outline_mode_edit_white_24dp;
-                imgExcluir = Resources.outline_delete_white_24dp;
-                imgFiltro = Resources.outline_filter_alt_white_24dp;
-                imgDevolver = Resources.devolver_white;
-                imgModo = Resources.darkMode;
+                SetarModoEscuro();
+        }
 
-                imgClose = Resources.close_dark;
-                corGrid = Color.FromArgb(77, 77, 77);
-                corDark = Color.FromArgb(26, 26, 26);
-                corFonte = Color.White;
-                corFundo = Color.FromArgb(51, 51, 51);
-                corFundoTxBox = Color.FromArgb(64, 64, 64);
-                corPanel = Color.FromArgb(38, 38, 38);
-                corTabela = Color.FromArgb(38, 38, 38);
-                ligado = false;
-            }
+        private static void SetarModoEscuro()
+        {
+            imgAdd = Resources.outline_add_circle_outline_white_24dp;
+            imgEditar = Resources.outline_mode_edit_white_24dp;
+            imgExcluir = Resources.outline_delete_white_24dp;
+            imgFiltro = Resources.outline_filter_alt_white_24dp;
+            imgDevolver = Resources.devolver_white;
+            imgModo = Resources.darkMode;
+
+            imgClose = Resources.close_dark;
+            corGrid = Color.FromArgb(77, 77, 77);
+            corDark = Color.FromArgb(26, 26, 26);
+            corFonte = Color.White;
+            corFundo = Color.FromArgb(51, 51, 51);
+            corFundoTxBox = Color.FromArgb(64, 64, 64);
+            corPanel = Color.FromArgb(38, 38, 38);
+            corTabela = Color.FromArgb(38, 38, 38);
+            ligado = false;
+        }
+
+        private static void SetarModoClaro()
+        {
+            imgAdd = Resources.outline_add_circle_outline_black_24dp;
+            imgEditar = Resources.outline_mode_edit_black_24dp;
+            imgExcluir = Resources.outline_delete_black_24dp;
+            imgFiltro = Resources.outline_filter_alt_black_24dp;
+            imgDevolver = Resources.devolver;
+            imgModo = Resources.whiteMode;
+
+            imgClose = Resources.close;
+            corGrid = Color.FromArgb(171, 171, 171);
+            corDark = Color.FromArgb(255, 255, 255);
+            corFonte = Color.Black;
+            corFundo = Color.FromArgb(242, 242, 242);
+            corFundoTxBox = Color.White;
+            corPanel = Color.FromArgb(191, 191, 191);
+            corTabela = Color.FromArgb(191, 191, 191);
+            ligado = true;
         }
     }
 }
