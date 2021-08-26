d2df8b038cc80fdccc7143a304971f1b69c9dffd
arthurmichaltchuk <arthurmichaltchuk4@gmail.com>
2021-08-26 17:39:31 -0300

    Descontao do Gabriel

--- LocadoraVeiculo/LocadoraVeiculo.WindowsApp/Features/DescontoFeature/TabelaDescontoControl.cs
+++ LocadoraVeiculo/LocadoraVeiculo.WindowsApp/Features/DescontoFeature/TabelaDescontoControl.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LocadoraVeiculo.DescontoModule;
+using LocadoraVeiculo.WindowsApp.Shared;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +12,59 @@ using System.Windows.Forms;
 
 namespace LocadoraVeiculo.WindowsApp.Features.DescontoFeature
 {
-    public partial class TabelaDescontoControl : UserControl
+    public partial class TabelaDescontoControl : UserControl, IApareciaAlteravel
     {
         public TabelaDescontoControl()
         {
             InitializeComponent();
+            ConfigurarGridLightMode();
+            gridDesconto.ConfigurarGridSomenteLeitura();
+            gridDesconto.Columns.AddRange(ObterColunas());
+        }
+
+        public void ConfigurarGridLightMode()
+        {
+            gridDesconto.ConfigurarGridZebrado();
+        }
+        public DataGridViewColumn[] ObterColunas()
+        {
+            var colunas = new DataGridViewColumn[]
+            {
+                new DataGridViewTextBoxColumn { DataPropertyName = "Id", HeaderText = "Id"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Valor", HeaderText = "Valor"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Validade", HeaderText = "Validade"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Parceiro", HeaderText = "Parceiro"},
+
+                new DataGridViewTextBoxColumn {DataPropertyName = "Meio", HeaderText = "Meio"},
+            };
+
+            return colunas;
+        }
+
+        public int ObtemIdSelecionado()
+        {
+            return gridDesconto.SelecionarId<int>();
+        }
+
+        public void AtualizarRegistros(List<Desconto> descontos)
+        {
+            gridDesconto.Rows.Clear();
+
+            foreach (Desconto item in descontos)
+            {
+                if (item.Tipo == "Porcentagem")
+                    gridDesconto.Rows.Add(item.Id, item.Valor+" %", item.Validade, item.Parceiro, item.Meio);
+                else
+                    gridDesconto.Rows.Add(item.Id, item.Valor, item.Validade, item.Parceiro, item.Meio);
+            }
+        }
+        
+        public void AtualizarAparencia()
+        {
+            ConfigurarGridLightMode();
         }
     }
 }
