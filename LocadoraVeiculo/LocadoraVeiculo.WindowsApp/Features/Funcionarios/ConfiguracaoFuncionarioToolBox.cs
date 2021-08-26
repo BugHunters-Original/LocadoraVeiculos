7a3823b6b1c61e8ae03ae32f02b5210332f5f6aa
arthurmichaltchuk <arthurmichaltchuk4@gmail.com>
2021-08-26 16:01:11 -0300

    funcionario ToolBox refatorado

--- LocadoraVeiculo/LocadoraVeiculo.WindowsApp/Features/Funcionarios/ConfiguracaoFuncionarioToolBox.cs
+++ LocadoraVeiculo/LocadoraVeiculo.WindowsApp/Features/Funcionarios/ConfiguracaoFuncionarioToolBox.cs
@@ -42,5 +42,11 @@ namespace LocadoraVeiculo.WindowsApp.Features.Funcionarios
         {
             get { return false; }
         }
+
+        public bool EnabledAdicionar => true;
+
+        public bool EnabledEditar => true;
+
+        public bool EnabledExcluir => true;
     }
 }
