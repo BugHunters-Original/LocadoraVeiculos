634d6b0e6cfecc80ea37d10027e3da91286a9b1c
arthurmichaltchuk <arthurmichaltchuk4@gmail.com>
2021-08-26 16:02:14 -0300

    Descontos criados

---
+++ LocadoraVeiculo/LocadoraVeiculo.Dominio/DescontoModule/Desconto.cs
@@ -0,0 +1,26 @@
+﻿using LocadoraVeiculo.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraVeiculo.DescontoModule
+{
+    public class Desconto : EntidadeBase
+    {
+
+        public Desconto()
+        {
+
+        }
+
+
+
+
+        public override string Validar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
