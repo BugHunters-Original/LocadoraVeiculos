b3c050823b7d07fca5f6cb4ad7d0bc7d34345110
GabrielMrqs <gabrielmrqs.contato@gmail.com>
2021-08-26 17:43:41 -0300

    Parceiro

---
+++ LocadoraVeiculo/LocadoraVeiculo.WindowsApp/Features/Parceiro/TabelaParceiroControl.cs
@@ -0,0 +1,57 @@
+﻿using LocadoraVeiculo.ParceiroModule;
+using LocadoraVeiculo.WindowsApp.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocadoraVeiculo.WindowsApp.Features.Parceiro
+{
+    public partial class TabelaParceiroControl : UserControl, IApareciaAlteravel
+    {
+        public TabelaParceiroControl()
+        {
+            InitializeComponent();
+            ConfigurarGridLightMode();
+            dgvParceiros.ConfigurarGridSomenteLeitura();
+            dgvParceiros.Columns.AddRange(ObterColunas());
+        }
+        public void ConfigurarGridLightMode()
+        {
+            dgvParceiros.ConfigurarGridZebrado();
+        }
+        public void AtualizarAparencia()
+        {
+            ConfigurarGridLightMode();
+        }
+        public int ObtemIdSelecionado()
+        {
+            return dgvParceiros.SelecionarId<int>();
+        }
+        public void AtualizarRegistros(List<ParceiroTaxa> parceiros)
+        {
+            dgvParceiros.Rows.Clear();
+
+            foreach (ParceiroTaxa parceiro in parceiros)
+            {
+                dgvParceiros.Rows.Add(parceiro.Id, parceiro.Nome);
+            }
+        }
+        public DataGridViewColumn[] ObterColunas()
+        {
+            var colunas = new DataGridViewColumn[]
+            {
+                new DataGridViewTextBoxColumn { DataPropertyName = "Id", HeaderText = "Id"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Nome", HeaderText = "Nome"}
+            };
+
+            return colunas;
+        }
+    }
+}
---
+++ LocadoraVeiculo/LocadoraVeiculo.WindowsApp/Features/Parceiro/TelaParceiroForm.cs
@@ -0,0 +1,50 @@
+﻿using LocadoraVeiculo.ParceiroModule;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocadoraVeiculo.WindowsApp.Features.Parceiro
+{
+    public partial class TelaParceiroForm : Form
+    {
+        private ParceiroTaxa parceiro;
+        public TelaParceiroForm()
+        {
+            InitializeComponent();
+        }
+
+        public ParceiroTaxa Parceiro
+        {
+            get { return Parceiro; }
+            set
+            {
+                parceiro = value;
+                txtID.Text = parceiro.Id.ToString();
+                txtNome.Text = parceiro.Nome;
+            }
+
+        }
+        private void btnGravar_Click(object sender, EventArgs e)
+        {
+            string nome = txtNome.Text;
+            string resultadoValidacao = parceiro.Validar();
+
+            if (resultadoValidacao != "ESTA_VALIDO")
+            {
+                string primeiroErro = new StringReader(resultadoValidacao).ReadLine();
+
+                TelaPrincipalForm.Instancia.AtualizarRodape(primeiroErro);
+
+                DialogResult = DialogResult.None;
+            }
+            parceiro = new ParceiroTaxa(nome);
+        }
+    }
+}
