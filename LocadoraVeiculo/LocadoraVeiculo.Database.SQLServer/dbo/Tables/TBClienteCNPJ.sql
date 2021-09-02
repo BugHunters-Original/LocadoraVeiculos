2289aecaf64244a89e56ef4d3764efb0e87c88ae
GabrielMrqs <gabrielmrqs.contato@gmail.com>
2021-09-01 23:37:35 -0300

    Alteração no Banco

--- LocadoraVeiculo/LocadoraVeiculo.Database.SQLServer/dbo/Tables/TBClienteCNPJ.sql
+++ LocadoraVeiculo/LocadoraVeiculo.Database.SQLServer/dbo/Tables/TBClienteCNPJ.sql
@@ -1,9 +1,12 @@
 ﻿CREATE TABLE [dbo].[TBClienteCNPJ] (
-    [Id]       INT          IDENTITY (1, 1) NOT NULL,
-    [Nome]     VARCHAR (50) NULL,
-    [Endereco] VARCHAR (50) NULL,
-    [Telefone] VARCHAR (50) NULL,
-    [CNPJ]     VARCHAR (50) NULL,
+    [Id]       INT           IDENTITY (1, 1) NOT NULL,
+    [Nome]     VARCHAR (50)  NULL,
+    [Endereco] VARCHAR (50)  NULL,
+    [Telefone] VARCHAR (50)  NULL,
+    [CNPJ]     VARCHAR (50)  NULL,
+    [Email]    VARCHAR (250) NULL,
     CONSTRAINT [PK_TBCliente] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
