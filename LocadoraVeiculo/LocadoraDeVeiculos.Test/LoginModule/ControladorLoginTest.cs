4fb53d53dd8ead91e2eff0c86f6fe32c3e4efa5d
arthurmichaltchuk <arthurmichaltchuk4@gmail.com>
2021-09-13 16:00:57 -0300

    TESTESSSS

---
+++ LocadoraVeiculo/LocadoraDeVeiculos.Test/LoginModule/ControladorLoginTest.cs
@@ -0,0 +1,83 @@
+﻿using FluentAssertions;
+using LocadoraDeVeiculos.Controladores.FuncionarioModule;
+using LocadoraDeVeiculos.Controladores.LoginModule;
+using LocadoraDeVeiculos.Controladores.Shared;
+using LocadoraDeVeiculos.Dominio.FuncionarioModule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Test.LoginModule
+{
+    [TestClass]
+    public class ControladorLoginTest
+    {
+        ControladorFuncionario controladorFuncionario = null;
+        ControladorLogin controlador = null;
+
+        public ControladorLoginTest()
+        {
+            controladorFuncionario = new ControladorFuncionario();
+            controlador = new ControladorLogin();
+            Db.Update("DELETE FROM [TBFUNCIONARIO]");
+
+        }
+
+        [TestMethod]
+        public void DeveRetornar_LoginValido()
+        {
+            //arrange
+            var novoFuncionario = new Funcionario("Luisa Farias", 3000, new DateTime(2021, 03, 03), "099.427.999-09", "luisa_f", "1234567");
+            controladorFuncionario.InserirNovo(novoFuncionario);
+
+            //action
+            var resultado = controlador.ValidarLogin("luisa_f", "1234567");
+
+            //assert
+            resultado.Should().Be("valido");
+        }
+
+        [TestMethod]
+        public void DeveRetornar_SenhaIncorreta()
+        {
+            //arrange
+            var novoFuncionario = new Funcionario("Luisa Farias", 3000, new DateTime(2021, 03, 03), "099.427.999-09", "luisa_f", "1234567");
+            controladorFuncionario.InserirNovo(novoFuncionario);
+
+            //action
+            var resultado = controlador.ValidarLogin("luisa_f", "00000");
+
+            //assert
+            resultado.Should().Be("Senha Incorreta");
+        }
+
+        [TestMethod]
+        public void DeveRetornar_UsuarioInexistente()
+        {
+            //arrange
+            var novoFuncionario = new Funcionario("Luisa Farias", 3000, new DateTime(2021, 03, 03), "099.427.999-09", "luisa_f", "1234567");
+            controladorFuncionario.InserirNovo(novoFuncionario);
+
+            //action
+            var resultado = controlador.ValidarLogin("luisa", "1234567");
+
+            //assert
+            resultado.Should().Be("Usuário Inexistente");
+        }
+
+        [TestMethod]
+        public void DeveRetornar_Valido_AdminAdmin()
+        {
+            //arrange
+
+            //action
+            var resultado = controlador.ValidarLogin("admin", "admin");
+
+            //assert
+            resultado.Should().Be("valido");
+        }
+    }
+}
