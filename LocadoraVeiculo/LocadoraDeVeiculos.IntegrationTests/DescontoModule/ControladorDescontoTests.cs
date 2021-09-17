f5a5e13ab6c0e3320f4bd65c0e87e460924a420e
Andrey Silva <andrey_silva13@outlook.com>
2021-09-17 16:49:13 -0300

    Integrations tests Desconto & Funcionario

---
+++ LocadoraVeiculo/LocadoraDeVeiculos.IntegrationTests/DescontoModule/ControladorDescontoTests.cs
@@ -0,0 +1,123 @@
+﻿using FluentAssertions;
+using LocadoraDeVeiculos.Dominio.DescontoModule;
+using LocadoraDeVeiculos.Dominio.ParceiroModule;
+using LocadoraDeVeiculos.Infra.Shared;
+using LocadoraDeVeiculos.Infra.SQL.DescontoModule;
+using LocadoraDeVeiculos.Infra.SQL.ParceiroModule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LocadoraDeVeiculos.Test.DescontoModule
+{
+    [TestClass]
+    public class ControladorDescontoTest
+    {
+        DescontoDAO descontoDAO = null;
+        ParceiroDAO parceiroDAO = null;
+
+        Parceiro parceiro;
+
+        public ControladorDescontoTest()
+        {
+            descontoDAO = new DescontoDAO();
+            parceiroDAO = new ParceiroDAO();
+            LimparBancos();
+
+            parceiro = new Parceiro("Arthur");
+            parceiroDAO.Inserir(parceiro);
+        }
+
+        private static void LimparBancos()
+        {
+            Db.Update("DELETE FROM [TBDESCONTO]");
+            Db.Update("DELETE FROM [TBPARCEIROS]");
+        }
+
+        [TestMethod]
+        public void DeveInserir_Desconto()
+        {
+            //arrange
+            var desconto = new Desconto("dinheiro", 50, "Porcentagem", new DateTime(2030, 10, 10), parceiro, "YouTube", "dimdim", 50);
+
+            //action
+            descontoDAO.Inserir(desconto);
+
+            //assert
+            var descontoEncontrado = descontoDAO.SelecionarPorId(desconto.Id);
+            descontoEncontrado.Should().Be(desconto);
+            LimparBancos();
+        }
+
+        [TestMethod]
+        public void DeveAtualizar_Desconto()
+        {
+            //arrange
+            var desconto = new Desconto("dinheiro", 50, "Porcentagem", new DateTime(2030, 10, 10), parceiro, "YouTube", "dimdim", 50);
+            descontoDAO.Inserir(desconto);
+            var descontoEditado = new Desconto("xuxu", 100, "Porcentagem", new DateTime(2030, 11, 11), parceiro, "Radio", "dimdim", 50);
+
+            //action
+            descontoDAO.Editar(desconto.Id, descontoEditado);
+
+            //assert
+            var descontoAtualizado = descontoDAO.SelecionarPorId(desconto.Id);
+            descontoAtualizado.Should().Be(descontoEditado);
+            LimparBancos();
+        }
+
+        [TestMethod]
+        public void DeveExcluir_Desconto()
+        {
+            //arrange            
+            var desconto = new Desconto("dinheiro", 50, "Porcentagem", new DateTime(2030, 10, 10), parceiro, "YouTube", "dimdim", 50);
+            descontoDAO.Inserir(desconto);
+
+            //action            
+            descontoDAO.Excluir(desconto.Id);
+
+            //assert
+            var descontoEncontrado = descontoDAO.SelecionarPorId(desconto.Id);
+            descontoEncontrado.Should().BeNull();
+            LimparBancos();
+        }
+
+        [TestMethod]
+        public void DeveSelecionar_Veiculo_PorId()
+        {
+            //arrange
+            var desconto = new Desconto("dinheiro", 50, "Porcentagem", new DateTime(2030, 10, 10), parceiro, "YouTube", "dimdim", 50);
+            descontoDAO.Inserir(desconto);
+
+            //action
+            var descontoEncontrado = descontoDAO.SelecionarPorId(desconto.Id);
+
+            //assert
+            descontoEncontrado.Should().NotBeNull();
+            LimparBancos();
+        }
+
+        [TestMethod]
+        public void DeveSelecionar_Todos()
+        {
+            //arrange
+            var d1 = new Desconto("dinheiro", 50, "Porcentagem", new DateTime(2030, 10, 10), parceiro, "YouTube", "dimdim", 50);
+            descontoDAO.Inserir(d1);
+
+            var d2 = new Desconto("xuxu", 50, "Porcentagem", new DateTime(2030, 10, 10), parceiro, "YouTube", "dimdim", 50);
+            descontoDAO.Inserir(d2);
+
+            var d3 = new Desconto("leilao", 50, "Porcentagem", new DateTime(2030, 10, 10), parceiro, "YouTube", "dimdim", 50);
+            descontoDAO.Inserir(d3);
+
+            //action
+            var veiculos = descontoDAO.SelecionarTodos();
+
+            //assert
+            veiculos.Should().HaveCount(3);
+            veiculos[0].Codigo.Should().Be("dinheiro");
+            veiculos[1].Codigo.Should().Be("xuxu");
+            veiculos[2].Codigo.Should().Be("leilao");
+            LimparBancos();
+        }
+    }
+}
---
+++ LocadoraVeiculo/LocadoraDeVeiculos.IntegrationTests/FuncionarioModule/ControladorFuncionarioTests.cs
@@ -0,0 +1,118 @@
+﻿using FluentAssertions;
+using LocadoraDeVeiculos.Dominio.FuncionarioModule;
+using LocadoraDeVeiculos.Infra.SQL.FuncionarioModule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LocadoraDeVeiculos.Test.FuncionarioModule
+{
+    [TestClass]
+    public class ControladorFuncionarioTests
+    {
+
+        FuncionarioDAO funcionarioDAO = null;
+
+        public ControladorFuncionarioTests()
+        {
+            funcionarioDAO = new FuncionarioDAO();
+            LimparBanco();
+
+        }
+
+        private static void LimparBanco()
+        {
+            Db.Update("DELETE FROM [TBFUNCIONARIO]");
+        }
+
+        [TestMethod]
+        public void DeveInserir_Funcionario()
+        {
+            //arrange
+            var novoFuncionario = new Funcionario("Luisa Farias", 3000, new DateTime(2021, 03, 03), "099.427.999-09", "luisa_f", "1234567");
+
+            //action
+            funcionarioDAO.Inserir(novoFuncionario);
+
+            //assert
+            var funcionarioEncontrado = funcionarioDAO.SelecionarPorId(novoFuncionario.Id);
+            funcionarioEncontrado.Should().Be(novoFuncionario);
+            LimparBanco();
+        }
+
+        [TestMethod]
+        public void DeveAtualizar_Funcionario()
+        {
+            //arrange
+            var funcionario = new Funcionario("Luisa Farias", 3000, new DateTime(2021, 03, 03), "099.427.999-09", "luisa_f", "1234567");
+            funcionarioDAO.Inserir(funcionario);
+
+            var novoFuncionario = new Funcionario("Luisa Farias", 4000, new DateTime(2021, 03, 03), "099.427.999-09", "luisa_f", "1234567");
+
+            //action
+            funcionarioDAO.Editar(funcionario.Id, novoFuncionario);
+
+            //assert
+            Funcionario funcionarioAtualizado = funcionarioDAO.SelecionarPorId(funcionario.Id);
+            funcionarioAtualizado.Should().Be(novoFuncionario);
+            LimparBanco();
+        }
+
+
+
+        [TestMethod]
+        public void DeveExcluir_Funcionario()
+        {
+            //arrange            
+            var funcionario = new Funcionario("Luisa Farias", 3000, new DateTime(2021, 03, 03), "099.427.999-09", "luisa_f", "1234567");
+            funcionarioDAO.Inserir(funcionario);
+
+            //action            
+            funcionarioDAO.Excluir(funcionario.Id);
+
+            //assert
+            Funcionario funcionarioEncontrado = funcionarioDAO.SelecionarPorId(funcionario.Id);
+            funcionarioEncontrado.Should().BeNull();
+            LimparBanco();
+        }
+
+        [TestMethod]
+        public void DeveSelecionar_Funcionario_PorId()
+        {
+            //arrange
+            var funcionario = new Funcionario("Luisa Farias", 3000, new DateTime(2021, 03, 03), "099.427.999-09", "luisa_f", "1234567");
+            funcionarioDAO.Inserir(funcionario);
+
+            //action
+            Funcionario funcionarioEncontrado = funcionarioDAO.SelecionarPorId(funcionario.Id);
+
+            //assert
+            funcionarioEncontrado.Should().NotBeNull();
+            LimparBanco();
+        }
+
+        [TestMethod]
+        public void DeveSelecionar_TodosFuncionarios()
+        {
+            //arrange
+            var f1 = new Funcionario("Luisa Farias", 3000, new DateTime(2021, 03, 03), "099.427.999-09", "luisa_f", "1234567");
+            funcionarioDAO.Inserir(f1);
+
+            var f2 = new Funcionario("Arthur", 6000, new DateTime(2021, 03, 03), "099.427.999-09", "user_arthur", "9999999999");
+            funcionarioDAO.Inserir(f2);
+
+            var f3 = new Funcionario("Andrey", 6000, new DateTime(2021, 03, 03), "099.427.999-09", "user_andrey", "88888898989");
+            funcionarioDAO.Inserir(f3);
+
+            //action
+            var funcionarios = funcionarioDAO.SelecionarTodos();
+
+            //assert
+            funcionarios.Should().HaveCount(3);
+            funcionarios[0].Nome.Should().Be("Andrey");
+            funcionarios[1].Nome.Should().Be("Arthur");
+            funcionarios[2].Nome.Should().Be("Luisa Farias");
+            LimparBanco();
+        }
+    }
+}
+
