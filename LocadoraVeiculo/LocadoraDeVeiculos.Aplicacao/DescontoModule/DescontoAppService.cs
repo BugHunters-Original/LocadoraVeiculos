42fcc2695d254276657b1a4c0f15b01abcbc511a
Andrey Silva <andrey_silva13@outlook.com>
2021-09-24 14:12:36 -0300

    Fix Desconto Logs

--- LocadoraVeiculo/LocadoraDeVeiculos.Aplicacao/DescontoModule/DescontoAppService.cs
+++ LocadoraVeiculo/LocadoraDeVeiculos.Aplicacao/DescontoModule/DescontoAppService.cs
@@ -20,45 +20,52 @@ namespace LocadoraDeVeiculos.Aplicacao.DescontoModule
         {
             string resultadoValidacaoDominio = desconto.Validar();
 
+            logger.Debug($"Registrando cupom de desconto {desconto.Nome}...");
+
             if (resultadoValidacaoDominio == "ESTA_VALIDO")
             {
-                logger.Debug($"Registrando cupom de desconto {desconto.Nome}...");
-
+                
                 descontoRepository.Inserir(desconto);
 
                 logger.Debug($"Cupom de desconto {desconto.Nome} registrado com sucesso!");
 
                 return true;
             }
+            else
+            {
+                logger.Error($"Não foi possível registrar o cupom de desconto: {desconto.Nome}.");
 
-            return false;
+                return false;
+            }
+            
         }
 
         public bool EditarDesconto(int id, Desconto desconto)
         {
             string resultadoValidacaoDominio = desconto.Validar();
 
+            logger.Debug($"Editando cupom de desconto {desconto.Nome}...");
+
             if (resultadoValidacaoDominio == "ESTA_VALIDO")
             {
-                logger.Debug($"Editando cupom de desconto {desconto.Nome}...");
-
                 descontoRepository.Editar(id, desconto);
 
                 logger.Debug($"Cupom de desconto {desconto.Nome} Editando com sucesso!");
 
                 return true;
             }
+            else
+            {
+                logger.Error($"Não foi possível editar o cupom de desconto: {desconto.Nome}.");
 
-            return false;
-        }
-
-        public List<Desconto> SelecionarPesquisa(string comboBox, string pesquisa)
-        {
-            return descontoRepository.SelecionarPesquisa(comboBox, pesquisa);
+                return false;
+            }        
         }
 
         public bool ExcluirDesconto(int id)
         {
+            logger.Debug($"Excluindo o desconto ID: {id}.");
+
             var desconto = descontoRepository.SelecionarPorId(id);
             var excluiu = descontoRepository.Excluir(id);
 
@@ -72,24 +79,28 @@ namespace LocadoraDeVeiculos.Aplicacao.DescontoModule
 
         public Desconto SelecionarPorId(int id)
         {
+            logger.Debug($"Selecionando o Cupom de desconto ID: {id}.");
+
             Desconto desconto =  descontoRepository.SelecionarPorId(id);
 
             if (desconto == null)
-                logger.Warn($"Não foi possível encontrar o desconto com ID: {id}");
+                logger.Warn($"Não foi possível encontrar o Cupom de desconto com ID: {id}");
             else
-                logger.Debug($"Selecionando desconto ID: {id}, NOME: {desconto.Nome}.");
+                logger.Debug($"Selecionou o Cupom de desconto ID: {id}, NOME: {desconto.Nome}.");
 
             return desconto;
         }
 
         public List<Desconto> SelecionarTodosDescontos()
         {
+            logger.Debug($"Selecionando todos os Cupons de descontos.");
+
             List<Desconto> desconto = descontoRepository.SelecionarTodos();
 
             if(desconto.Count == 0)
-                logger.Warn($"Não há descontos cadastrados");
+                logger.Warn($"Não há Cupons de descontos cadastrados");
             else
-                logger.Debug($"Selecionando todos os {desconto.Count} descontos.");
+                logger.Debug($"Selecionou os {desconto.Count} Cupons de descontos existentes.");
 
             return desconto;
         }
@@ -101,6 +112,11 @@ namespace LocadoraDeVeiculos.Aplicacao.DescontoModule
         public Desconto VerificarCodigoValido(string codigo)
         {
             return descontoRepository.VerificarCodigoValido(codigo);
+        }
+
+        public List<Desconto> SelecionarPesquisa(string comboBox, string pesquisa)
+        {
+            return descontoRepository.SelecionarPesquisa(comboBox, pesquisa);
         }
     }
 }
