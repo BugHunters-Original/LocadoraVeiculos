92d4a12c40da4e939129834b5e0cb53c628da7f9
arthurmichaltchuk <arthurmichaltchuk4@gmail.com>
2021-10-06 17:36:33 -0300

    Arthur.

---
+++ LocadoraVeiculo/LocadoraDeVeiculos.Infra.ORM/ServicoModule/ServicoDAO.cs
@@ -0,0 +1,20 @@
+﻿using LocadoraDeVeiculos.Dominio.ServicoModule;
+using LocadoraDeVeiculos.Infra.Context;
+using LocadoraDeVeiculos.Infra.ORM.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Infra.ORM.ServicoModule
+{
+    public class ServicoDAO : BaseDAO<Servico>, IServicoRepository
+    {
+        public ServicoDAO(LocacaoContext context) : base(context)
+        {
+
+        }
+
+    }
+}
