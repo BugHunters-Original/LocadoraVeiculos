24b4a29e590573bdeb36b2204d9c5292dd891357
Andrey Silva <andrey_silva13@outlook.com>
2021-10-06 16:54:38 -0300

    Commit do Andrey - Configuration

---
+++ LocadoraVeiculo/LocadoraDeVeiculos.Infra.Configurations/DescontoModule/DescontoConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using LocadoraDeVeiculos.Dominio.DescontoModule;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LocadoraDeVeiculos.Infra.Configurations.DescontoModule
+{
+    public class DescontoConfiguration : IEntityTypeConfiguration<Desconto>
+    {
+        public void Configure(EntityTypeBuilder<Desconto> builder)
+        {
+            builder.ToTable("TBDescontos");
+
+            builder.HasKey(desconto => desconto.Id);
+
+            builder.Property(desconto => desconto.Codigo).HasColumnType("VARCHAR(50)").IsRequired();
+
+            builder.Property(desconto => desconto.Valor).HasColumnType("FLOAT").IsRequired();
+
+            builder.Property(desconto => desconto.Tipo).HasColumnType("VARCHAR(50)").IsRequired();
+
+            builder.Property(desconto => desconto.Validade).HasColumnType("DATE").IsRequired();
+
+            builder.Property(desconto => desconto.Meio).HasColumnType("VARCHAR(50)").IsRequired();
+
+            builder.Property(desconto => desconto.Nome).HasColumnType("VARCHAR(50)").IsRequired();
+
+            builder.Property(desconto => desconto.ValorMinimo).HasColumnType("FLOAT").IsRequired();
+
+            builder.Property(desconto => desconto.Usos).HasColumnType("INT").IsRequired();
+
+            builder.HasOne(desconto => desconto.Parceiro)
+                .WithMany(desconto => desconto.Descontos)
+                .HasForeignKey(desconto => desconto.IdParceiro);
+        }
+    }
+}
