3f446938792671879377321cb08fd33f6eda96ea
Andrey Silva <andrey_silva13@outlook.com>
2021-10-07 14:36:30 -0300

    Implem. DAO

---
+++ LocadoraVeiculo/LocadoraDeVeiculos.Infra.ORM/DescontoModule/DescontoDAO.cs
@@ -0,0 +1,25 @@
+﻿using LocadoraDeVeiculos.Dominio.DescontoModule;
+using LocadoraDeVeiculos.Infra.Context;
+using LocadoraDeVeiculos.Infra.ORM.Shared;
+using System.Linq;
+
+namespace LocadoraDeVeiculos.Infra.ORM.DescontoModule
+{
+    public class DescontoDAO : BaseDAO<Desconto>, IDescontoRepository
+    {
+        public DescontoDAO(LocacaoContext contexto) : base(contexto)
+        {
+
+        }
+
+        public bool VerificarCodigoExistente(string codigo)
+        {
+            return contexto.Descontos.ToList().Exists(d => d.Codigo == codigo);
+        }
+
+        public Desconto VerificarCodigoValido(string codigo)
+        {
+            return contexto.Descontos.ToList().Find(d => d.Codigo == codigo);
+        }
+    }
+}
---
+++ LocadoraVeiculo/LocadoraDeVeiculos.Infra.ORM/FuncionarioModule/FuncionarioDAO.cs
@@ -0,0 +1,18 @@
+﻿using LocadoraDeVeiculos.Dominio.FuncionarioModule;
+using LocadoraDeVeiculos.Infra.Context;
+using LocadoraDeVeiculos.Infra.ORM.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Infra.ORM.FuncionarioModule
+{
+    class FuncionarioDAO : BaseDAO<Funcionario>, IFuncionarioRepository
+    {
+        public FuncionarioDAO(LocacaoContext contexto) : base(contexto)
+        {
+        }
+    }
+}
